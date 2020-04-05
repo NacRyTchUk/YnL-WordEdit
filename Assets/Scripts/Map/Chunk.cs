61ff87041e18865926491724f6e857a451d3a6aa
NacRyTchUk <god2god3@mail.ru>
2020-04-05 12:54:49 +0700

    end st 1

---
+++ Assets/Scripts/Map/Chunk.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Assets.Scripts
+{
+    public class Chunk
+    {
+        public  const int CHUNK_WIGHT = 48, CHUNK_HEIGHT = 24, MIN_LAYER_VALUE = 5, MAX_LAYER_VALUE = 5;
+        private GameObject[] _layersHeaders = new GameObject[MAX_LAYER_VALUE + MIN_LAYER_VALUE + 1];
+        private Vector2 _coordOfTheChunk;
+        private List<Block> _listOfBlocks = new List<Block>();
+
+        private GameObject _parentOfChunk;
+
+
+        public string Dir { get; set; }
+
+        public static int MinLayerValue
+        {
+            get { return MIN_LAYER_VALUE; }
+        }
+
+        public GameObject[] LayersHeaders
+        {
+            get { return _layersHeaders; }
+        }
+
+
+        public void SetCoord(int x, int y)
+        {
+            _coordOfTheChunk.x = x;
+            _coordOfTheChunk.y = y;
+        }
+
+        public void SetCoord(Vector2 coord)
+        {
+            _coordOfTheChunk = coord;
+        }
+
+        public Vector2 GetCoord()
+        {
+            return _coordOfTheChunk;
+        }
+
+        private void LoadOnScreen()
+        {
+            
+            var chunkHeader = Object.Instantiate((GameObject) Resources.Load("Empty"), _parentOfChunk.transform);
+            chunkHeader.transform.position = new Vector3(_coordOfTheChunk.x*CHUNK_WIGHT,_coordOfTheChunk.y*CHUNK_HEIGHT);
+            chunkHeader.name = _coordOfTheChunk.x + "," + _coordOfTheChunk.y;
+
+            foreach (var lob in _listOfBlocks)
+            {
+                
+                var layerValue = lob.mainLayer + MIN_LAYER_VALUE;
+                if (_layersHeaders[layerValue] == null)
+                {
+                    _layersHeaders[layerValue] =
+                        Object.Instantiate((GameObject) Resources.Load("Empty"), chunkHeader.transform);
+                    _layersHeaders[layerValue].name = "Layer: " + lob.mainLayer;
+                }
+
+                var newObj = Object.Instantiate((GameObject) Resources.Load("Empty"),
+                    _layersHeaders[layerValue].transform);
+                newObj.name = "[" + lob.blockIndex + ";" + lob.x + ";" + lob.y + ";" + lob.mainLayer + "]";
+
+                newObj.AddComponent<SpriteRenderer>().sprite = MapFileSystem.pic[lob.blockIndex];
+
+                newObj.transform.position += new Vector3(lob.x, lob.y, lob.mainLayer);
+            }
+        }
+
+
+        private void LoadInMemory()
+        {
+            using (var sr = new StreamReader(Dir + "/" + _coordOfTheChunk.x + ',' + _coordOfTheChunk.y + ".chunk"))
+            {
+                var fileLine = sr.ReadLine();
+                if (fileLine == null) throw new NullReferenceException();
+
+                var mainBlock = fileLine.Split('$');
+                var bricks = mainBlock[0].Split(':');
+                foreach (var t in bricks)
+                {
+                    var oneBrick = t.Replace("[", "").Replace("]", "");
+                    var brickData = oneBrick.Split(';');
+
+                    Block newBlock;
+                    newBlock.blockIndex = Convert.ToInt32(brickData[0]);
+                    newBlock.x = Convert.ToSingle(brickData[1]);
+                    newBlock.y = Convert.ToSingle(brickData[2]);
+                    newBlock.mainLayer = Convert.ToInt32(brickData[3]);
+                    _listOfBlocks.Add(newBlock);
+                }
+            }
+        }
+
+        public void Load([NotNull] GameObject pom)
+        {
+            _parentOfChunk = pom;
+            LoadInMemory();
+            LoadOnScreen();
+        }
+    }
+}
---
+++ Assets/Scripts/Map/ChunkLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnassignedField.Global
+
+namespace Assets.Scripts
+{
+    public class ChunkLoader : MonoBehaviour
+    {
+        private List<Chunk> _listOfChunks = new List<Chunk>();
+        private GameObject _map;
+        
+        public List<Chunk> ListOfChunks
+        {
+            get { return _listOfChunks; }
+            set { _listOfChunks = value; }
+        }
+
+        public void LoadAllChunks()
+        {
+            _map = new GameObject("Map");
+            foreach (var loc in _listOfChunks)
+            {
+                loc.Load(_map);
+            }
+            
+            _map.transform.position = new Vector3(-0.5f,-0.5f);
+        }
+
+    }
+}
